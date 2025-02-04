902163f4ad5bc90bf752310694137b8097d2cf1d
Yuliya Bondarenko <juliyaodessa84@gmail.com>
2025-02-04 10:20:50 +0100

    form-register form-login

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,9 +1,34 @@
-import { NavLink } from "react-router-dom";
+import { Formik, Form, Field } from "formik";
+import { Link } from "react-router-dom";
 
 const LogIn = () => {
+  const initialValues = {
+    password: "",
+    email: "",
+  };
+
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+    actions.resetForm();
+  };
   return (
     <>
-      <NavLink>Log In</NavLink>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <Form>
+          <label>
+            <span>Email:</span>
+            <Field name="email" />
+          </label>
+          <label>
+            <span>Password:</span>
+            <Field name="password" type="password" />
+          </label>
+          <button type="submit">Log In</button>
+          <p>
+            You do not have an account?<Link to="/register">Go to create!</Link>
+          </p>
+        </Form>
+      </Formik>
     </>
   );
 };
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,39 @@
-import { NavLink } from "react-router-dom";
+import { Formik, Form, Field } from "formik";
+import { Link } from "react-router-dom";
 
 const Register = () => {
+  const initialValues = {
+    password: "",
+    email: "",
+    name: "",
+  };
+
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+    actions.resetForm();
+  };
   return (
     <>
-      <NavLink>Register</NavLink>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <Form>
+          <label>
+            <span>Name:</span>
+            <Field name="name" />
+          </label>
+          <label>
+            <span>Email:</span>
+            <Field name="email" />
+          </label>
+          <label>
+            <span>Password:</span>
+            <Field name="password" type="password" />
+          </label>
+          <button type="submit">Register</button>
+          <p>
+            You already have an account?<Link to="/login">Login!</Link>
+          </p>
+        </Form>
+      </Formik>
     </>
   );
 };
